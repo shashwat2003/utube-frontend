7ce592a10a4e0870ff75f137ab8b6bfbe7f25383
shashwat2003 <info.superpcstudio@gmail.com>
2023-11-18 15:38:29 +0000

    🎉 init: initial project struc

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type {Metadata} from "next";
+import {Poppins} from "next/font/google";
+import "./globals.css";
+import "./tailwind.css";
+
+const poppins = Poppins({weight: ["400"]});
+
+export const metadata: Metadata = {
+  title: "U-Tube",
+  description: "Video streaming platform for developers.",
+};
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
---
+++ commitlint.config.ts
@@ -0,0 +1,51 @@
+import type {UserConfig} from "@commitlint/types";
+
+// Ref:
+// https://github.com/ccnnde/commitlint-config-git-commit-emoji/blob/master/index.js
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  parserPreset: {
+    parserOpts: {
+      headerPattern:
+        /^(?<type>.*\s\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))$/,
+      headerCorrespondence: ["type", "scope", "subject"],
+    },
+  },
+  rules: {
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+    "header-max-length": [2, "always", 72],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-exclamation-mark": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "🎉 init",
+        "✨ feat",
+        "🐞 fix",
+        "📃 docs",
+        "🌈 style",
+        "🦄 refactor",
+        "🎈 perf",
+        "🧪 test",
+        "🔧 build",
+        "🐎 ci",
+        "🐳 chore",
+        "↩ revert",
+      ],
+    ],
+  },
+};
+
+module.exports = config;
